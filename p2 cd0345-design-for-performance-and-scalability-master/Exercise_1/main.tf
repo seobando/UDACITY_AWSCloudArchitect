f6a560e831d19df240ca942ff3e81375ee27448e
Sebastian OM <sebastianobandomorales@gmail.com>
2024-06-22 17:08:57 -0500

    starting p2

---
+++ p2 cd0345-design-for-performance-and-scalability-master/Exercise_1/main.tf
@@ -0,0 +1,25 @@
+# TODO: Designate a cloud provider, region, and credentials
+provider "aws" {
+  region  = "ap-southeast-2"
+  profile = "default"
+}
+
+# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
+resource "aws_instance" "udacity_t2" {
+  count         = 4
+  ami           = "ami-0c55b159cbfafe1f0" # Example AMI ID, change as needed
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Udacity T2"
+  }
+}
+
+# TODO: provision 2 m4.large EC2 instances named Udacity M4
+resource "aws_instance" "udacity_m4" {
+  count         = 2
+  ami           = "ami-0c55b159cbfafe1f0" # Example AMI ID, change as needed
+  instance_type = "m4.large"
+  tags = {
+    Name = "Udacity M4"
+  }
+}
