dcf60c68c4b653bd065c75fd7728f448c2959659
Sebastian OM <sebastianobandomorales@gmail.com>
2024-06-22 18:32:02 -0500

    cleaning

--- p2 cd0345-design-for-performance-and-scalability-master/Exercise_1/main.tf
+++ p2 cd0345-design-for-performance-and-scalability-master/Exercise_1/main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
 resource "aws_instance" "udacity_t2" {
   count         = 4
-  ami           = "ami-0323c3dd2da7fb37d" # Example AMI ID, change as needed
+  ami           = "ami-0323c3dd2da7fb37d"
   instance_type = "t2.micro"
   tags = {
     Name = "Udacity T2"
@@ -18,7 +18,7 @@ resource "aws_instance" "udacity_t2" {
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
 resource "aws_instance" "udacity_m4" {
   count         = 2
-  ami           = "ami-0c55b159cbfafe1f0" # Example AMI ID, change as needed
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "m4.large"
   tags = {
     Name = "Udacity M4"
