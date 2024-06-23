2f6123ecaee3dfdfe4b8bf9e4c1fdbef82d8ccb6
Sebastian OM <sebastianobandomorales@gmail.com>
2024-06-22 19:14:54 -0500

    update

--- p2 cd0345-design-for-performance-and-scalability-master/Exercise_1/main.tf
+++ p2 cd0345-design-for-performance-and-scalability-master/Exercise_1/main.tf
@@ -5,14 +5,14 @@ variable "region" {
 # TODO: Designate a cloud provider, region, and credentials
 provider "aws" {
   region                   = var.region
-  shared_credentials_files = "../credentials"
+  shared_credentials_files = ["../credentials"]
   profile                  = "default"
 }
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
 resource "aws_instance" "udacity_t2" {
   count         = 4
-  ami           = "ami-0323c3dd2da7fb37d"
+  ami           = "ami-0806bc468ce3a22ec"
   instance_type = "t2.micro"
   tags = {
     Name = "Udacity T2"
@@ -22,7 +22,7 @@ resource "aws_instance" "udacity_t2" {
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
 resource "aws_instance" "udacity_m4" {
   count         = 2
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0bd01824d64912730"
   instance_type = "m4.large"
   tags = {
     Name = "Udacity M4"
