271ec1f5b74d1a4d9df7c5344991bae87cba5481
Sebastian OM <sebastianobandomorales@gmail.com>
2024-06-22 20:18:40 -0500

    wip

--- p2 cd0345-design-for-performance-and-scalability-master/Exercise_2/main.tf
+++ p2 cd0345-design-for-performance-and-scalability-master/Exercise_2/main.tf
@@ -4,6 +4,33 @@ provider "aws" {
   profile                  = "default"
 }
 
+
+# IAM role for Lambda execution
+resource "aws_iam_role" "lambda_exec_role" {
+  name               = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  })
+}
+
+
+# IAM policy attachment for Lambda execution role (allow logging)
+resource "aws_iam_role_policy_attachment" "lambda_exec_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+
 # Lambda function resource
 resource "aws_lambda_function" "greet_lambda" {
   filename      = var.output_file_name
@@ -20,30 +47,6 @@ resource "aws_lambda_function" "greet_lambda" {
   }
 }
 
-# IAM role for Lambda execution
-resource "aws_iam_role" "lambda_exec_role" {
-  name               = "lambda_exec_role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-# IAM policy attachment for Lambda execution role (allow logging)
-resource "aws_iam_role_policy_attachment" "lambda_exec_attach" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
 
 # Package the Lambda function code
 data "archive_file" "lambda_zip" {
@@ -51,9 +54,4 @@ data "archive_file" "lambda_zip" {
   source_dir  = "${path.module}"
   output_path = "${path.module}/lambda.zip"
   depends_on  = [aws_iam_role_policy_attachment.lambda_exec_attach]
-}
-
-# Output the Lambda function ARN
-output "lambda_function_arn" {
-  value = aws_lambda_function.lambda_function.arn
 }
